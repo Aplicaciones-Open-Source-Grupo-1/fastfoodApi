59187850bf2a66273deb712ce775dfc6c0a5134e
Charlie <u202120841@upc.edu.pe>
2023-11-24 16:01:13 -0500

    first commit

---
+++ src/test/java/fastfood/api/fastfood/FastfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package fastfood.api.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
